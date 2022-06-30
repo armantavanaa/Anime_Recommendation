0ed8f17de63c5de17597b3afd736a66091373833
lawrence <lawrencedlin@gmail.com>
2022-06-30 07:32:37 +0000

    Problem was in multiprocessing sampler w/e

--- utils.py
+++ utils.py
@@ -14,35 +14,71 @@ def random_neq(l, r, s):
         t = np.random.randint(l, r)
     return t
 
+def sample_function_one(user_train, usernum, itemnum, batch_size, maxlen, SEED):
+    def sample():
+
+        user = np.random.randint(1, usernum + 1)
+        while True:
+            try:
+                while len(user_train[user]) <= 1: user = np.random.randint(1, usernum + 1)
+
+                seq = np.zeros([maxlen], dtype=np.int32)
+                pos = np.zeros([maxlen], dtype=np.int32)
+                neg = np.zeros([maxlen], dtype=np.int32)
+                nxt = user_train[user][-1]
+                idx = maxlen - 1
+
+                ts = set(user_train[user])
+                for i in reversed(user_train[user][:-1]):
+                    seq[idx] = i
+                    pos[idx] = nxt
+                    if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)
+                    nxt = i
+                    idx -= 1
+                    if idx == -1: break
+
+                # print(user, seq, pos, neg)
+                return (user, seq, pos, neg)
+            except KeyError:
+                continue
+
+    np.random.seed(SEED)
+    return sample()
 
 def sample_function(user_train, usernum, itemnum, batch_size, maxlen, result_queue, SEED):
     def sample():
 
         user = np.random.randint(1, usernum + 1)
-        while len(user_train[user]) <= 1: user = np.random.randint(1, usernum + 1)
+        while True:
+            try:
+                while len(user_train[user]) <= 1: user = np.random.randint(1, usernum + 1)
 
-        seq = np.zeros([maxlen], dtype=np.int32)
-        pos = np.zeros([maxlen], dtype=np.int32)
-        neg = np.zeros([maxlen], dtype=np.int32)
-        nxt = user_train[user][-1]
-        idx = maxlen - 1
+                seq = np.zeros([maxlen], dtype=np.int32)
+                pos = np.zeros([maxlen], dtype=np.int32)
+                neg = np.zeros([maxlen], dtype=np.int32)
+                nxt = user_train[user][-1]
+                idx = maxlen - 1
 
-        ts = set(user_train[user])
-        for i in reversed(user_train[user][:-1]):
-            seq[idx] = i
-            pos[idx] = nxt
-            if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)
-            nxt = i
-            idx -= 1
-            if idx == -1: break
+                ts = set(user_train[user])
+                for i in reversed(user_train[user][:-1]):
+                    seq[idx] = i
+                    pos[idx] = nxt
+                    if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)
+                    nxt = i
+                    idx -= 1
+                    if idx == -1: break
 
-        return (user, seq, pos, neg)
+                # print(user, seq, pos, neg)
+                return (user, seq, pos, neg)
+            except KeyError:
+                continue
 
     np.random.seed(SEED)
     while True:
         one_batch = []
         for i in range(batch_size):
-            one_batch.append(sample())
+            b = sample()
+            one_batch.append(b)
 
         result_queue.put(zip(*one_batch))
 
